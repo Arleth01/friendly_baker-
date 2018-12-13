a46be451247c2a52a5100b4787980c5ace5e4ba4
Arleth <Aontiveros@nyc.yearup.org>
2018-12-13 15:24:58 -0500

    project

---
+++ public/main.js
@@ -0,0 +1,14 @@
+let the = document.getElementById('cookie')
+the.addEventListener('click', function(){
+    document.getElementById('display').style.display='block';
+});
+
+let c = document.getElementById('cheese')
+c.addEventListener('click', function(){
+    document.getElementById('show').style.display='block';
+});
+
+let m = document.getElementById('muffin')
+m.addEventListener('click', function(){
+    document.getElementById('appear').style.display='block';
+});
--- server.rb
+++ server.rb
@@ -21,17 +21,43 @@ require 'sinatra/reloader'
 
 
 
-@@chocolate = Cookies.new("chocolate", "75")
+@@Ch0 = Cookies.new("chocolate", "0.75")
 @@cookie1 = Cookies.new("gingersnap", "1.00")
 @@cookie2 = Cookies.new("oatmeal", "1.25")
 @@cookie3 = Cookies.new("peanutbutter", "1.25")
+@@Ch = @@Ch0.type
+@@ch = @@Ch0.price
+@@Cg = @@cookie1.type
+@@Cg1 = @@cookie1.price
+@@Co = @@cookie2.type
+@@ccp = @@cookie2.price
+@@cop = @@cookie3.type
+@@co1 = @@cookie3.price 
 
 @@blueberry = Muffin.new("blurberry", "2.00")   
 @@b1 = Muffin.new("applecinnamon", "1.50")
 @@b2 = Muffin.new("banana", "1.25")
 @@b3 = Muffin.new("coffeecake", "2.50")
+@@bb = @@blueberry.type
+@@bb1 = @@blueberry.price
+@@bbt = @@b1.type
+@@bbp = @@b1.price
+@@bb2 = @@b2.type
+@@bp = @@b2.price
+@@bt3 = @@b3.type
+@@bp3 = @@b3.price
+
 get '/' do 
     
     erb :index
+
+end 
+
+get '/about' do 
+     erb :about 
+end 
+
+get '/contact' do
+    erb :contact
 
 end 
